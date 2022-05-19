fdd8795eeea8957ef14784be424c04d9ae4682a3
Damian Lim <damianlim@Damians-Air.lan>
2022-05-18 22:10:15 -0400

    v1 setup

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 //Hello
+// This file will hold our actual extension script
+const allImgs = document.querySelectorAll('img');
+const testImg = document.createElement('img');
+testImg.src = 'https://assets.website-files.com/5ee147cc310949c7d83f7f9d/5f30a34fa1634899b41fda8e_codesmith-course-page.png'
+console.log(allImgs)
+allImgs.forEach(node => node.src = testImg.src);
